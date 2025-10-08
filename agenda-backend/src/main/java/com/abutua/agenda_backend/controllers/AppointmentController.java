bec1dfba2ac3a14b96585a08cf1f3ab1bf6f91d6
Arthur Motter <arthur.motter@fatec.sp.gov.br>
2025-10-08 12:11:27 -0300

    feat(appointment): core files and changes

---
+++ agenda-backend/src/main/java/com/abutua/agenda_backend/controllers/AppointmentController.java
@@ -0,0 +1,38 @@
+package com.abutua.agenda_backend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.abutua.agenda_backend.dtos.AppointmentRequest;
+import com.abutua.agenda_backend.dtos.AppointmentResponse;
+import com.abutua.agenda_backend.services.AppointmentService;
+
+
+@RestController
+@RequestMapping("appointments")
+public class AppointmentController {
+
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @PostMapping
+    public ResponseEntity<AppointmentResponse> createAppointment(@Validated @RequestBody AppointmentRequest appointmentRequest) {
+
+        var appointmentResponse = appointmentService.createAppointment(appointmentRequest);
+
+        var location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(appointmentResponse.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(appointmentResponse);
+    }
+
+}
---
+++ agenda-backend/src/main/java/com/abutua/agenda_backend/controllers/AppointmentTypeController.java
@@ -0,0 +1,27 @@
+package com.abutua.agenda_backend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.abutua.agenda_backend.dtos.AppointmentTypeResponse;
+import com.abutua.agenda_backend.services.AppointmentService;
+
+
+@RestController
+@RequestMapping("appointment-types")
+public class AppointmentTypeController {
+
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @GetMapping
+    public ResponseEntity<List<AppointmentTypeResponse>> getAppointmentTypes(){
+        return ResponseEntity.ok().body(appointmentService.getAllTypes());
+    }
+    
+}
